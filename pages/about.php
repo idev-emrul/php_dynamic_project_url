42fc11ed7bf6b66bc33a7360d475f38867aecb6c
idev-emrul <emrul.hasan@itlabsolutions.com>
2023-09-07 15:28:49 +0600

    seperate base code and required file

--- pages/about.php
+++ pages/about.php
@@ -3,10 +3,12 @@
     // =====================================================================================
     isset(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))[1]) ? $project_folder = explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))[1] : $project_folder = 'your_project_folder_name';
     include_once($_SERVER['DOCUMENT_ROOT'] . '/' . $project_folder . '/em_config.php');
-        
-    // --------- INCLUDE HEADER ---------
-    include(app_url('inc/header.php'));
     // ========= END COMMON CODE ============================================================
+?>
+
+<?php
+    // --------- INCLUDE REQUIRED FILE LIKE HEADER ---------
+    include(app_url('inc/header.php'));
 ?>
 
 <!-- Your specific page content goes here -->
--- pages/company/about_c.php
+++ pages/company/about_c.php
@@ -2,13 +2,13 @@
     // ################ COMMON CODE FOR EACH PAGE, FOR DYNAMIC DIRECTORY ACCESS ############
     // =====================================================================================
     isset(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))[1]) ? $project_folder = explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))[1] : $project_folder = 'your_project_folder_name';
-    if (!file_exists('em_config.php')) {
-        include_once($_SERVER['DOCUMENT_ROOT'] . '/' . $project_folder . '/em_config.php');
-    }
-        
-    // --------- INCLUDE HEADER ---------
-    include(app_url('inc/header.php'));
+    include_once($_SERVER['DOCUMENT_ROOT'] . '/' . $project_folder . '/em_config.php');
     // ========= END COMMON CODE ============================================================
+?>
+
+<?php
+    // --------- INCLUDE REQUIRED FILE LIKE HEADER ---------
+    include(app_url('inc/header.php'));
 ?>
 
 
